34a34fcaa488ac4866f716799ba5eb6269504db4
m1st <m.a.stepanov@outlook.com>
2024-12-03 01:20:11 +0300

    Added Postgres to AppHost.

--- EventManager.AppHost/Program.cs
+++ EventManager.AppHost/Program.cs
@@ -3,7 +3,11 @@ using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres");
+var postgresDb = postgres.AddDatabase("postgresDb");
+
 var webapi = builder.AddProject<Projects.EventManager_WebApi>("webapi")
+    .WithReference(postgresDb)
     .WithExternalHttpEndpoints();
 
 var frontend = builder.AddNpmApp("frontend", "../eventmanager.frontend")
