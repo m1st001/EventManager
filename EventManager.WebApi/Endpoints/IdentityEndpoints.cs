b5068e16f9c5dcbd383a02e9f9865c97fccb0e0c
Alexandr <105442577+spooon122@users.noreply.github.com>
2024-12-12 21:14:38 +0300

    Added base endpoints for authentication

--- EventManager.WebApi/Endpoints/IdentityEndpoints.cs
+++ EventManager.WebApi/Endpoints/IdentityEndpoints.cs
@@ -1,9 +1,23 @@
-﻿namespace EventManager.WebApi.Endpoints;
+﻿using EventManager.WebApi.Data.Models.Requests;
+using EventManager.WebApi.Services.Abstractions;
+
+namespace EventManager.WebApi.Endpoints;
 
 public static class IdentityEndpoints
 {
     public static void RegisterIdentityEndpoints(this WebApplication app)
     {
+        var auth = app.MapGroup("authentication").WithOpenApi();
+
+        auth.MapPost("login",
+            async (IAuthenticationService authenticationService, LoginRequest request, bool rememberMe) =>
+                await authenticationService.Login(request.Username, request.Password, rememberMe));
+
+        auth.MapPost("register",
+            async (IAuthenticationService authenticationService, RegisterRequest request) =>
+                await authenticationService.Register(request.Username, request.Email, request.Password));
         
+        auth.MapPost("logout", async (IAuthenticationService authenticationService) =>
+            await authenticationService.Logout());
     }
 }
