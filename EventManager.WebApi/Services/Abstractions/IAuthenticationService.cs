1cd75638c4451998842175951dbb621ef22f9a8a
Alexandr <105442577+spooon122@users.noreply.github.com>
2024-12-12 21:05:31 +0300

    Added new authentication service

---
+++ EventManager.WebApi/Services/Abstractions/IAuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using EventManager.WebApi.Data.Models;
+
+namespace EventManager.WebApi.Services.Abstractions;
+
+public interface IAuthenticationService
+{
+    /// <summary>
+    /// register in system
+    /// </summary>
+    /// <param name="username">Username profile</param>
+    /// <param name="email">Email profile</param>
+    /// <param name="password">Password profile</param>
+    /// <returns>User model</returns>
+    Task<User> Register(string username, string? email, string password);
+
+    /// <summary>
+    /// Login in system
+    /// </summary>
+    /// <param name="username">Username</param>
+    /// <param name="password">User password</param>
+    /// <param name="useCookies">Flag for cookies</param>
+    /// <returns></returns>
+    Task<User?> Login(string username, string password, bool useCookies);
+
+    /// <summary>
+    /// Logout from system
+    /// </summary>
+    /// <returns></returns>
+    Task Logout();
+}
---
+++ EventManager.WebApi/Services/AuthenticationService.cs
@@ -0,0 +1,58 @@
+﻿using EventManager.WebApi.Data.Models;
+using EventManager.WebApi.Services.Abstractions;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventManager.WebApi.Services;
+
+/// <inheritdoc cref="IAuthenticationService"/>
+public class AuthenticationService(
+    UserManager<User> userManager,
+    SignInManager<User> signInManager,
+    ILogger<AuthenticationService> logger) : IAuthenticationService
+{
+    private readonly UserManager<User> _userManager = userManager;
+    private readonly SignInManager<User> _signInManager = signInManager;
+    private readonly ILogger _logger = logger;
+
+    /// <inheritdoc cref="IAuthenticationService.Register"/>
+    public async Task<User> Register(string username, string? email, string password)
+    {
+        var existedUser = await _userManager.FindByNameAsync(username);
+        if (existedUser is not null)
+        {
+            _logger.LogError("User {} already exists", username);
+            return existedUser;
+        }
+
+        var user = new User
+        {
+            UserName = username,
+            Email = email
+        };
+
+        await _userManager.CreateAsync(user, password);
+
+        return user;
+    }
+
+    /// <inheritdoc cref="IAuthenticationService.Login"/>
+    public async Task<User?> Login(string username, string password, bool useCookies)
+    {
+        var user = await _userManager.FindByNameAsync(username);
+        if (user is null)
+        {
+            _logger.LogError("User {username} not found", username);
+            return null;
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(user, password, useCookies, false);
+
+        return result.Succeeded ? user : null;
+    }
+
+    /// <inheritdoc cref="IAuthenticationService.Logout"/>
+    public async Task Logout()
+    {
+        await _signInManager.SignOutAsync();
+    }
+}
