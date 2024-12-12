b2f4ec415e7feb80061caea66bd0a4c128fbbcfb
Alexandr <105442577+spooon122@users.noreply.github.com>
2024-12-12 21:04:52 +0300

    Added models for base authentication

---
+++ EventManager.WebApi/Data/Models/Requests/LoginRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace EventManager.WebApi.Data.Models.Requests;
+
+/// <summary>
+/// Request for login endpoint
+/// </summary>
+public record LoginRequest
+{
+    /// <summary>
+    /// Username
+    /// </summary>
+    public required string Username { get; set; }
+
+    /// <summary>
+    /// User password
+    /// </summary>
+    public required string Password { get; set; }
+
+    /// <summary>
+    /// Remember me flag
+    /// </summary>
+    public bool RememberMe { get; set; } = false;
+}
