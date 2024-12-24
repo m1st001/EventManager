9b59aa4d5539a463d52a52bf9b24d4ecd631919e
Mihail <m.a.stepanov@outlook.com>
2024-12-25 01:58:45 +0300

    Added some User-related endpoints.

---
+++ EventManager.WebApi/Endpoints/UserEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using EventManager.WebApi.Services.Abstractions;
+
+namespace EventManager.WebApi.Endpoints;
+
+public static class UserEndpoints
+{
+    public static void RegisterUserEndpoints(this WebApplication app)
+    {
+        var users = app.MapGroup("/users").WithOpenApi().WithTags("Users");
+        
+        users.MapGet("/{id:int}", async (int id, IUserService userService)
+            => TypedResults.Ok(await userService.GetUserByIdAsync(id)));
+        
+        users.MapGet("/{name}", async (string name, IUserService userService)
+            => TypedResults.Ok(await userService.GetUserByNameAsync(name)));
+    }
+}
