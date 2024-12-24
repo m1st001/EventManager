33dc0844bdce59031254fd0f774ba606d99637bf
Alexandr <105442577+spooon122@users.noreply.github.com>
2024-12-24 23:32:47 +0300

    Removed (IResult)TypedResult from response

--- EventManager.WebApi/Endpoints/IdentityEndpoints.cs
+++ EventManager.WebApi/Endpoints/IdentityEndpoints.cs
@@ -15,20 +15,16 @@ public static class IdentityEndpoints
         auth.MapPost("login",
             async (IAuthenticationService authenticationService, LoginRequest request, bool rememberMe) =>
             {
-                var user = await authenticationService.Login(request.Username, request.Password,
-                    rememberMe); // Login user
-                if (user is not null)
-                {
-                    return TypedResults.Ok("Successfully logged in");
-                }
-
-                return (IResult)TypedResults.BadRequest("Username or password is incorrect");
+                var user = await authenticationService.Login(request.Username, request.Password, rememberMe); // Login user
+                return user is not null
+                    ? TypedResults.Ok("Successfully logged in")
+                    : Results.BadRequest("Username or password is incorrect");
             });
 
         auth.MapPost("register", async (IAuthenticationService authenticationService, RegisterRequest request) =>
         {
             await authenticationService.Register(request.Username, request.Email, request.Password); // Register user
-            
+
             return TypedResults.Ok("Successfully registered");
         });
 
