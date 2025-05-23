0bb7156a81228bf926b0b50600965c20c081616c
Mihail <m.a.stepanov@outlook.com>
2025-05-24 02:24:25 +0300

    Reworked SubscribedEventsTab.tsx

--- EventManager.Frontend/src/store/thunks/authThunk.ts
+++ EventManager.Frontend/src/store/thunks/authThunk.ts
@@ -1,40 +1,57 @@
-import {createAsyncThunk} from '@reduxjs/toolkit';
-import {LoginRequest, RegisterRequest} from "../../api/data-contracts.ts";
-import {authClient} from "../../api/apiConfig.ts";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { LoginRequest, RegisterRequest } from "../../api/data-contracts.ts";
+import { authClient } from "../../api/apiConfig.ts";
 
-export const loginUser = createAsyncThunk('auth/login', async (credentials: LoginRequest, {rejectWithValue}) => {
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (credentials: LoginRequest, { rejectWithValue }) => {
     try {
-        const response = await authClient.loginCreate({rememberMe: true}, credentials);
-        localStorage.setItem('authUser', JSON.stringify(response.data));
-        return response.data;
+      const response = await authClient.loginCreate(
+        { rememberMe: true },
+        credentials,
+      );
+      localStorage.setItem("authUser", JSON.stringify(response.data));
+      return response.data;
     } catch (error: any) {
-        return rejectWithValue(error.response?.data || 'Login failed');
+      return rejectWithValue(error.response?.data || "Login failed");
     }
-});
+  },
+);
 
-export const registerUser = createAsyncThunk('auth/register', async (credentials: RegisterRequest, {rejectWithValue}) => {
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (credentials: RegisterRequest, { rejectWithValue }) => {
     try {
-        return await authClient.registerCreate(credentials);
+      const response = await authClient.registerCreate(credentials);
+      localStorage.setItem("authUser", JSON.stringify(response.data));
+      return response.data;
     } catch (error: any) {
-        return rejectWithValue(error.response?.data || 'Registration failed');
+      return rejectWithValue(error.response?.data || "Registration failed");
     }
-});
+  },
+);
 
-export const logout = createAsyncThunk('auth/logout', async (_, {rejectWithValue}) => {
+export const logout = createAsyncThunk(
+  "auth/logout",
+  async (_, { rejectWithValue }) => {
     try {
-        await authClient.logoutCreate();
-        localStorage.removeItem('authUser');
-        return null;
+      await authClient.logoutCreate();
+      localStorage.removeItem("authUser");
+      return null;
     } catch (error: any) {
-        return rejectWithValue('Logout failed');
+      return rejectWithValue("Logout failed");
     }
-});
+  },
+);
 
-export const fetchMe = createAsyncThunk('auth/me', async (_, {rejectWithValue}) => {
+export const fetchMe = createAsyncThunk(
+  "auth/me",
+  async (_, { rejectWithValue }) => {
     try {
-        const response = await authClient.getAuthentication();
-        return response.data;
+      const response = await authClient.getAuthentication();
+      return response.data;
     } catch (error: any) {
-        return rejectWithValue('Fetch user data failed');
+      return rejectWithValue("Fetch user data failed");
     }
-});
+  },
+);
